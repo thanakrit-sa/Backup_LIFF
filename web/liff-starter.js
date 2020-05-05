ae8e465ec6458afb0b0e6df61606c66f3ced915a
thanakrit <thanakrit.10197@gmail.com>
2020-05-05 21:23:57 +0700

    liff

--- web/liff-starter.js
+++ web/liff-starter.js
@@ -1,6 +1,5 @@
-/// event เมื่อโหลดหน้าเพจเรียบร้อยแล้ว ให้ใช้คำสั่ง liff.init() สำหรับเตรียมความพร้อมใช้คำสั่ง LIFF ต่างๆ
 window.onload = function () {
-    liff.init(function () { // คำสั่ง init() คืนค่าข้อมูลของ LIFF app 
-        initializeApp(); // เมื่อ liff พร้อมทำงาน เรียกฟังก์ชั่น initializeApp ส่งข้อมูล LIFF ไปแสดง
+    liff.init(function () { 
+        
     });
 };
