178a1991b6f7e39f3779bba99c2afda0d2daf07a
thanakrit <thanakrit.10197@gmail.com>
2020-05-05 22:36:20 +0700

    liff

--- chatbot.php
+++ chatbot.php
@@ -148,125 +148,68 @@ $noword = new BubbleContainerBuilder(
 );
 $notext = new BubbleContainerBuilder(
     '{
-        
-        "type": "flex",
-        "altText": "Flex Message",
-        "contents": {
-          "type": "bubble",
-          "direction": "ltr",
-          "header": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "text",
-                "text": "Purchase",
-                "size": "lg",
-                "align": "start",
-                "weight": "bold",
-                "color": "#009813"
-              },
-              {
-                "type": "text",
-                "text": "฿ 100.00",
-                "size": "3xl",
-                "weight": "bold",
-                "color": "#000000"
-              },
-              {
-                "type": "text",
-                "text": "Rabbit Line Pay",
-                "size": "lg",
-                "weight": "bold",
-                "color": "#000000"
-              },
-              {
-                "type": "text",
-                "text": "2019.02.14 21:47 (GMT+0700)",
-                "size": "xs",
-                "color": "#B2B2B2"
-              },
-              {
-                "type": "text",
-                "text": "Payment complete.",
-                "margin": "lg",
-                "size": "lg",
-                "color": "#000000"
-              }
-            ]
-          },
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "separator",
-                "color": "#C3C3C3"
-              },
-              {
-                "type": "box",
-                "layout": "baseline",
-                "margin": "lg",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "Merchant",
-                    "align": "start",
-                    "color": "#C3C3C3"
-                  },
-                  {
-                    "type": "text",
-                    "text": "BTS 01",
-                    "align": "end",
-                    "color": "#000000"
-                  }
-                ]
-              },
-              {
-                "type": "box",
-                "layout": "baseline",
-                "margin": "lg",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "New balance",
-                    "color": "#C3C3C3"
-                  },
-                  {
-                    "type": "text",
-                    "text": "฿ 45.57",
-                    "align": "end"
-                  }
-                ]
-              },
-              {
-                "type": "separator",
-                "margin": "lg",
-                "color": "#C3C3C3"
-              }
-            ]
-          },
-          "footer": {
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
-              {
-                "type": "text",
-                "text": "View Details",
-                "size": "lg",
-                "align": "start",
-                "color": "#0084B6",
-                "action": {
-                  "type": "uri",
-                  "label": "View Details",
-                  "uri": "https://google.co.th/"
+        "type": "carousel",
+        "contents": [
+          {
+            "type": "bubble",
+            "body": {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                  "wrap": true
                 }
-              }
-            ]
+              ]
+            },
+            "footer": {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "button",
+                  "style": "primary",
+                  "action": {
+                    "type": "uri",
+                    "label": "Go",
+                    "uri": "https://example.com"
+                  }
+                }
+              ]
+            }
+          },
+          {
+            "type": "bubble",
+            "body": {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "Hello, World!",
+                  "wrap": true
+                }
+              ]
+            },
+            "footer": {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "button",
+                  "style": "primary",
+                  "action": {
+                    "type": "uri",
+                    "label": "Go",
+                    "uri": "https://example.com"
+                  }
+                }
+              ]
+            }
           }
-        }
-    
-    }'
+        ]
+      }'
 );
 
 if (!is_null($events)) {
