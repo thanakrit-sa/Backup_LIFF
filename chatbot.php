309cd5cb5325af572c61b758d28e2f6febdad141
thanakrit <thanakrit.10197@gmail.com>
2020-05-05 22:57:41 +0700

    liff

--- chatbot.php
+++ chatbot.php
@@ -344,12 +344,34 @@ if (!is_null($events)) {
             }
             break;
     }
-}
 
-$response = $bot->replyMessage($replyToken, $replyData);
-if ($response->isSucceeded()) {
-    echo 'Succeeded!';
-    return;
-}
 
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+// $response = $bot->replyMessage($replyToken, $replyData);
+// if ($response->isSucceeded()) {
+//     echo 'Succeeded!';
+//     return;
+// }
+
+// echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+$url = 'https://api.line.me/v2/bot/message/reply';
+    $data = [
+      'replyToken' => $replyToken,
+      'messages' => [$messages],
+    ];
+    $post = json_encode($data);
+    $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    echo $result . "\r\n";
+
+};
+
+
