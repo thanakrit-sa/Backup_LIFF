0c340301d44c6a9d5bf48aaa63a2d76439a74ba9
thanakrit <thanakrit.10197@gmail.com>
2020-05-05 22:15:13 +0700

    liff

--- chatbot.php
+++ chatbot.php
@@ -157,6 +157,124 @@ $notext = new BubbleContainerBuilder(
         )
     )
 );
+$jdata = '{
+    "type": "flex",
+    "altText": "Flex Message",
+    "contents": {
+      "type": "bubble",
+      "direction": "ltr",
+      "header": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "Purchase",
+            "size": "lg",
+            "align": "start",
+            "weight": "bold",
+            "color": "#009813"
+          },
+          {
+            "type": "text",
+            "text": "฿ 100.00",
+            "size": "3xl",
+            "weight": "bold",
+            "color": "#000000"
+          },
+          {
+            "type": "text",
+            "text": "Rabbit Line Pay",
+            "size": "lg",
+            "weight": "bold",
+            "color": "#000000"
+          },
+          {
+            "type": "text",
+            "text": "2019.02.14 21:47 (GMT+0700)",
+            "size": "xs",
+            "color": "#B2B2B2"
+          },
+          {
+            "type": "text",
+            "text": "Payment complete.",
+            "margin": "lg",
+            "size": "lg",
+            "color": "#000000"
+          }
+        ]
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "separator",
+            "color": "#C3C3C3"
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "margin": "lg",
+            "contents": [
+              {
+                "type": "text",
+                "text": "Merchant",
+                "align": "start",
+                "color": "#C3C3C3"
+              },
+              {
+                "type": "text",
+                "text": "BTS 01",
+                "align": "end",
+                "color": "#000000"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "margin": "lg",
+            "contents": [
+              {
+                "type": "text",
+                "text": "New balance",
+                "color": "#C3C3C3"
+              },
+              {
+                "type": "text",
+                "text": "฿ 45.57",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "separator",
+            "margin": "lg",
+            "color": "#C3C3C3"
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "text",
+            "text": "View Details",
+            "size": "lg",
+            "align": "start",
+            "color": "#0084B6",
+            "action": {
+              "type": "uri",
+              "label": "View Details",
+              "uri": "https://google.co.th/"
+            }
+          }
+        ]
+      }
+    }
+}';
 if (!is_null($events)) {
     $userMessage = strtolower($userMessage);
     if (!is_null($eventFollow)) {
@@ -176,125 +294,7 @@ if (!is_null($events)) {
             if ($userMessage != null) {
                 if ($userMessage == "liff") {
                     $replyData = new FlexMessageBuilder(
-                        "ข้อความตอบกลับ",
-                        '{
-                            "type": "flex",
-                            "altText": "Flex Message",
-                            "contents": {
-                              "type": "bubble",
-                              "direction": "ltr",
-                              "header": {
-                                "type": "box",
-                                "layout": "vertical",
-                                "contents": [
-                                  {
-                                    "type": "text",
-                                    "text": "Purchase",
-                                    "size": "lg",
-                                    "align": "start",
-                                    "weight": "bold",
-                                    "color": "#009813"
-                                  },
-                                  {
-                                    "type": "text",
-                                    "text": "฿ 100.00",
-                                    "size": "3xl",
-                                    "weight": "bold",
-                                    "color": "#000000"
-                                  },
-                                  {
-                                    "type": "text",
-                                    "text": "Rabbit Line Pay",
-                                    "size": "lg",
-                                    "weight": "bold",
-                                    "color": "#000000"
-                                  },
-                                  {
-                                    "type": "text",
-                                    "text": "2019.02.14 21:47 (GMT+0700)",
-                                    "size": "xs",
-                                    "color": "#B2B2B2"
-                                  },
-                                  {
-                                    "type": "text",
-                                    "text": "Payment complete.",
-                                    "margin": "lg",
-                                    "size": "lg",
-                                    "color": "#000000"
-                                  }
-                                ]
-                              },
-                              "body": {
-                                "type": "box",
-                                "layout": "vertical",
-                                "contents": [
-                                  {
-                                    "type": "separator",
-                                    "color": "#C3C3C3"
-                                  },
-                                  {
-                                    "type": "box",
-                                    "layout": "baseline",
-                                    "margin": "lg",
-                                    "contents": [
-                                      {
-                                        "type": "text",
-                                        "text": "Merchant",
-                                        "align": "start",
-                                        "color": "#C3C3C3"
-                                      },
-                                      {
-                                        "type": "text",
-                                        "text": "BTS 01",
-                                        "align": "end",
-                                        "color": "#000000"
-                                      }
-                                    ]
-                                  },
-                                  {
-                                    "type": "box",
-                                    "layout": "baseline",
-                                    "margin": "lg",
-                                    "contents": [
-                                      {
-                                        "type": "text",
-                                        "text": "New balance",
-                                        "color": "#C3C3C3"
-                                      },
-                                      {
-                                        "type": "text",
-                                        "text": "฿ 45.57",
-                                        "align": "end"
-                                      }
-                                    ]
-                                  },
-                                  {
-                                    "type": "separator",
-                                    "margin": "lg",
-                                    "color": "#C3C3C3"
-                                  }
-                                ]
-                              },
-                              "footer": {
-                                "type": "box",
-                                "layout": "horizontal",
-                                "contents": [
-                                  {
-                                    "type": "text",
-                                    "text": "View Details",
-                                    "size": "lg",
-                                    "align": "start",
-                                    "color": "#0084B6",
-                                    "action": {
-                                      "type": "uri",
-                                      "label": "View Details",
-                                      "uri": "https://google.co.th/"
-                                    }
-                                  }
-                                ]
-                              }
-                            }
-                        }'
+                        "ข้อความตอบกลับ", $jdata
                     );
                 } else {
                     $replyData = new FlexMessageBuilder("ข้อความตอบกลับ", $noword);
