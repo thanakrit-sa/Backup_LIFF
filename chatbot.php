915416a58be463b386dde2cbe9be30f17a8dbf2b
thanakrit <thanakrit.10197@gmail.com>
2020-05-06 09:36:27 +0700

    liff

--- chatbot.php
+++ chatbot.php
@@ -19,122 +19,52 @@ $content = file_get_contents('php://input');
 
 
 $jsonFlex = [
-    "type" => "flex",
-    "altText" => "Hello Flex Message",
-    "contents" => [
-      "type" => "bubble",
-      "direction" => "ltr",
-      "header" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "Purchase",
-            "size" => "lg",
-            "align" => "start",
-            "weight" => "bold",
-            "color" => "#009813"
-          ],
-          [
-            "type" => "text",
-            "text" => "฿ 100.00",
-            "size" => "3xl",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => "Rabbit Line Pay",
-            "size" => "lg",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => "2019.02.14 21:47 (GMT+0700)",
-            "size" => "xs",
-            "color" => "#B2B2B2"
-          ],
-          [
-            "type" => "text",
-            "text" => "Payment complete.",
-            "margin" => "lg",
-            "size" => "lg",
-            "color" => "#000000"
-          ]
-        ]
-      ],
-      "body" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "separator",
-            "color" => "#C3C3C3"
-          ],
-          [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "Merchant",
-                "align" => "start",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => "BTS 01",
-                "align" => "end",
-                "color" => "#000000"
+    
+        "type"=> "template",
+        "altText"=> "this is a carousel template",
+        "template"=> [
+          "type"=> "carousel",
+          "actions"=> [],
+          "columns"=> [
+            [
+              "thumbnailImageUrl"=> "SPECIFY_YOUR_IMAGE_URL",
+              "title"=> "Product1 ",
+              "text"=> "Description",
+              "actions"=> [
+                [
+                  "type"=> "uri",
+                  "label"=> "Detail 1",
+                  "uri"=> "https://liff.line.me/1654173341-pJegLPb3"
               ]
-            ]
-          ],
-          [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "New balance",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => "฿ 45.57",
-                "align" => "end"
               ]
-            ]
           ],
-          [
-            "type" => "separator",
-            "margin" => "lg",
-            "color" => "#C3C3C3"
+            [
+              "thumbnailImageUrl"=> "SPECIFY_YOUR_IMAGE_URL",
+              "title"=> "Product 2",
+              "text"=> "Description",
+              "actions"=> [
+                [
+                  "type"=> "message",
+                  "label"=> "Detail 2",
+                  "text"=> "https://liff.line.me/1654173341-pJegLPb3"
+              ]
+              ]
+          ],
+            [
+              "thumbnailImageUrl"=> "SPECIFY_YOUR_IMAGE_URL",
+              "title"=> "Product 3",
+              "text"=> "Description",
+              "actions"=> [
+                [
+                  "type"=> "message",
+                  "label"=> "Detail 3",
+                  "text"=> "https://liff.line.me/1654173341-pJegLPb3"
+              ]
+              ]
           ]
-        ]
-      ],
-      "footer" => [
-        "type" => "box",
-        "layout" => "horizontal",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "View Details",
-            "size" => "lg",
-            "align" => "start",
-            "color" => "#0084B6",
-            "action" => [
-              "type" => "uri",
-              "label" => "View Details",
-              "uri" => "https://google.co.th/"
-            ]
           ]
-        ]
-      ]
-    ]
+]
+      
   ];
 
 
