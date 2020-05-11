387d5a078814fb9fb2455078dc97df59b3185ef2
thanakrit <thanakrit.10197@gmail.com>
2020-05-11 16:44:19 +0700

    Rich Manu Postman

--- chatbot.php
+++ chatbot.php
@@ -16,15 +16,12 @@ foreach ($request_array['events'] as $event) {
 function file_get_contents_curl($url)
 {
   $ch = curl_init();
-
   curl_setopt($ch, CURLOPT_HEADER, 0);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_URL, $url);
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-
   $data = curl_exec($ch);
   curl_close($ch);
-
   return $data;
 }
 
@@ -60,7 +57,6 @@ function send_reply_message($url, $post_header, $post_body)
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
   $result = curl_exec($ch);
   curl_close($ch);
-
   return $result;
 }
 
@@ -68,13 +64,11 @@ function send_reply_message($url, $post_header, $post_body)
 $url = 'https://api.line.me/v2/bot/richmenu';
 $post_rich = json_encode($jsonRich, JSON_UNESCAPED_UNICODE);
 $ch = curl_init();
-
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_HTTPHEADER, $POST_HEADER);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post_rich);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
 $response = curl_exec($ch);
 curl_close($ch);
 echo $response;
