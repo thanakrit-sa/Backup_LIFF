298036d3241f854d2eb6e26c6dfc1c6dfbf7ed89
thanakrit <thanakrit.10197@gmail.com>
2020-05-11 18:11:03 +0700

    New File Flex_Message

--- chatbot.php
+++ chatbot.php
@@ -47,14 +47,6 @@ if ($message == "แสดงสินค้า") {
   $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
   $send_result = send_reply_message($API_URL . '/reply', $POST_HEADER, $post_body);
 }
-else {
-  $data = [
-    'replyToken' => $reply_token,
-    'messages' => 'Not Match Keyword'
-  ];
-  $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-  $send_result = send_reply_message($API_URL . '/reply', $POST_HEADER, $post_body);
-}
 
 // if ($message == "แสดงสินค้า") {
 //   $data = [
