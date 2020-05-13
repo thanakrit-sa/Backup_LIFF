8360ad1ea40d90f3e5f9051e75bbb5418b30c8da
thanakrit <thanakrit.10197@gmail.com>
2020-05-13 18:11:11 +0700

    Modified Flex

--- web/Product/product_1.php
+++ web/Product/product_1.php
@@ -68,11 +68,11 @@ foreach ($dataFromApi['data'] as $data) {
                 </div>
                 <div class="row">
                     <div class="col-4" align="right"><b>Stock : </b></div>
-                    <div class="col-8" align="left"><? echo $prod_stock[1];?></div>
+                    <div class="col-8" align="left"><? echo $prod_stock[1]." ชิ้น";?></div>
                 </div>
                 <div class="row">
                     <div class="col-4" align="right"><b>Price : </b></div>
-                    <div class="col-8" align="left"><? echo $prod_price[1];?></div>
+                    <div class="col-8" align="left"><? echo $prod_price[1]." บาท";?></div>
                 </div>
             </div>
         </div>
