8b34427944e16e617f5542d3462c66f787ff161f
thanakrit <thanakrit.10197@gmail.com>
2020-05-13 17:17:55 +0700

    Modified Flex

--- web/Product/product_1.php
+++ web/Product/product_1.php
@@ -47,7 +47,7 @@ foreach ($dataFromApi['data'] as $data) {
             <div clss="col">
                 <div class="row">
                     <div class="col p-0">
-                        <img src=<?"$prod_image[0]"?>>
+                        <img src="https://e-sport.in.th/ssdev/ecom/dashboard/uploads/img_prod/prod_img_1589361641.jpg">
                     </div>
                 </div>
             </div>
