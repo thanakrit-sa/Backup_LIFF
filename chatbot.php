f77a813dbd664230db03e456037a5c34edb05566
thanakrit <thanakrit.10197@gmail.com>
2020-05-13 16:11:26 +0700

    Modified Flex

--- chatbot.php
+++ chatbot.php
@@ -25,19 +25,20 @@ function file_get_contents_curl($url)
   return $data;
 }
 
-$api = file_get_contents_curl("https://e-sport.in.th/ssdev/ecom/dashboard/api/products/");
-// $html = file_get_contents_curl("http://dummy.restapiexample.com/api/v1/employees"); #API Dummy
+// $api = file_get_contents_curl("https://e-sport.in.th/ssdev/ecom/dashboard/api/products/");
+$html = file_get_contents_curl("http://dummy.restapiexample.com/api/v1/employees"); #API Dummy
 $dataFromApi = json_decode($api, true);
 
 foreach ($dataFromApi['data'] as $data) {
-  $prod_name[] = $data['product_name'];
-  $prod_image[] = $data['image_path'];
-  $prod_stock[] = $data['stock'];
-  $prod_price[] = $data['price'];
-  $prod_address[] = $data['address'];
-  $prod_cate[] = $data['category_name'];
-  $prod_created_time[] = $data['created_at'];
-  $prod_updated_time[] = $data['updated_at'];
+  // $prod_name[] = $data['product_name'];
+  // $prod_image[] = $data['image_path'];
+  // $prod_stock[] = $data['stock'];
+  // $prod_price[] = $data['price'];
+  // $prod_address[] = $data['address'];
+  // $prod_cate[] = $data['category_name'];
+  // $prod_created_time[] = $data['created_at'];
+  // $prod_updated_time[] = $data['updated_at'];
+  echo $dataFromApi['data'];
 }
 
 # Flex Message
