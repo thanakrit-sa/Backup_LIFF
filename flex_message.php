536d67233a36b9dffac348a1df4795cd92c05fdc
thanakrit <thanakrit.10197@gmail.com>
2020-05-13 16:09:48 +0700

    Modified Flex

--- flex_message.php
+++ flex_message.php
@@ -1,31 +1,31 @@
 <?
-function file_get_contents_curl($url)
-{
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_HEADER, 0);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-  $data = curl_exec($ch);
-  curl_close($ch);
-  return $data;
-}
+// function file_get_contents_curl($url)
+// {
+//   $ch = curl_init();
+//   curl_setopt($ch, CURLOPT_HEADER, 0);
+//   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//   curl_setopt($ch, CURLOPT_URL, $url);
+//   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+//   $data = curl_exec($ch);
+//   curl_close($ch);
+//   return $data;
+// }
 
-// $api = file_get_contents_curl("https://e-sport.in.th/ssdev/ecom/dashboard/api/products/");
-$html = file_get_contents_curl("http://dummy.restapiexample.com/api/v1/employees"); #API Dummy
-$dataFromApi = json_decode($api, true);
+// // $api = file_get_contents_curl("https://e-sport.in.th/ssdev/ecom/dashboard/api/products/");
+// $html = file_get_contents_curl("http://dummy.restapiexample.com/api/v1/employees"); #API Dummy
+// $dataFromApi = json_decode($api, true);
 
-foreach ($dataFromApi['data'] as $data) {
-//   $prod_name[] = $data['product_name'];
-//   $prod_image[] = $data['image_path'];
-//   $prod_stock[] = $data['stock'];
-//   $prod_price[] = $data['price'];
-//   $prod_address[] = $data['address'];
-//   $prod_cate[] = $data['category_name'];
-//   $prod_created_time[] = $data['created_at'];
-//   $prod_updated_time[] = $data['updated_at'];
-echo $dataFromApi['data'];
-}
+// foreach ($dataFromApi['data'] as $data) {
+// //   $prod_name[] = $data['product_name'];
+// //   $prod_image[] = $data['image_path'];
+// //   $prod_stock[] = $data['stock'];
+// //   $prod_price[] = $data['price'];
+// //   $prod_address[] = $data['address'];
+// //   $prod_cate[] = $data['category_name'];
+// //   $prod_created_time[] = $data['created_at'];
+// //   $prod_updated_time[] = $data['updated_at'];
+// echo $dataFromApi['data'];
+// }
 
 // for($i = 0;$i < count($dataFromApi['data']);$i++) {
 //     echo $prod_name[0];
