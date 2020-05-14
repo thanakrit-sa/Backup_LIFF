c98775c80370e7172fcfbbc847fece0cabd0adf0
thanakrit <thanakrit.10197@gmail.com>
2020-05-14 18:45:43 +0700

    now

--- web/order_history.php
+++ web/order_history.php
@@ -113,8 +113,8 @@
             </div>
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-5">
-                        <img src="top.jpg" id="img-content" class="img-fluid p-0 m-0">
+                    <div class="col-5" align="right">
+                        <img src="top.jpg" id="img-content" class="img-thumbnail p-0 m-0">
                     </div>
                     <article class="col-7" align="left">
                         <b>Name : </b> Nike Air Jordan 1S <br>
