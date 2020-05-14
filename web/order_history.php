782040272dbc05c445e42a2c501803cadf927e56
thanakrit <thanakrit.10197@gmail.com>
2020-05-14 18:03:39 +0700

    now

--- web/order_history.php
+++ web/order_history.php
@@ -37,46 +37,54 @@
                             </article>
                             <div id="received" class="col-1 p-0 m-0"></div>
                         </div>
-                        <br>
-                        <div class="row p-0 m-0">
-                            <div class="col-3 p-0 m-0">
-                                <img src="top.jpg" id="img-content" class="img-fluid p-0 m-0">
-                            </div>
-                            <article class="col-8 py-2" align="left">
-                                <b>Receipt</b> RTXY-02214445332 <br>
-                                <b>Date</b> 20/12/2060 12:45 AM<br>
-                                <b>Status</b> Sending
-                            </article>
-                            <div id="sending" class="col-1 p-0 m-0"></div>
-                        </div>
-                        <br>
-                        <div class="row p-0 m-0">
-                            <div class="col-3 p-0 m-0">
-                                <img src="top.jpg" id="img-content" class="img-fluid p-0 m-0">
-                            </div>
-                            <article class="col-8 py-2" align="left">
-                                <b>Receipt</b> RTXY-02214445332 <br>
-                                <b>Date</b> 20/12/2060 12:45 AM<br>
-                                <b>Status</b> Cancel
-                            </article>
-                            <div id="cancel" class="col-1 p-0 m-0"></div>
-                        </div>
-                        <hr>
-                        <nav aria-label="Page navigation example">
-                            <ul class="pagination justify-content-center">
-                                <li class="page-item disabled">
-                                    <a class="page-link" href="#" tabindex="-1">Previous</a>
-                                </li>
-                                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">Next</a>
-                                </li>
-                            </ul>
-                        </nav>
                     </div>
                 </div>
+                <br>
+                <div class="card">
+                    <div class="card-body p-0 m-0">
+                        <div class="row p-0 m-0">
+                            <div class="col-3 p-0 m-0">
+                                <img src="top.jpg" id="img-content" class="img-fluid p-0 m-0">
+                            </div>
+                            <article class="col-8 py-2" align="left">
+                                <b>Receipt</b> RTXY-02214445332 <br>
+                                <b>Date</b> 20/12/2060 12:45 AM<br>
+                                <b>Status</b> Received
+                            </article>
+                            <div id="received" class="col-1 p-0 m-0"></div>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="card">
+                    <div class="card-body p-0 m-0">
+                        <div class="row p-0 m-0">
+                            <div class="col-3 p-0 m-0">
+                                <img src="top.jpg" id="img-content" class="img-fluid p-0 m-0">
+                            </div>
+                            <article class="col-8 py-2" align="left">
+                                <b>Receipt</b> RTXY-02214445332 <br>
+                                <b>Date</b> 20/12/2060 12:45 AM<br>
+                                <b>Status</b> Received
+                            </article>
+                            <div id="received" class="col-1 p-0 m-0"></div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination justify-content-center">
+                        <li class="page-item disabled">
+                            <a class="page-link" href="#" tabindex="-1">Previous</a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">Next</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </div>
