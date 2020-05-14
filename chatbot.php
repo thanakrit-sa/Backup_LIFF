cbf75b0d830a9eaef74b9619e47297c4c76f54e7
thanakrit <thanakrit.10197@gmail.com>
2020-05-14 11:45:00 +0700

    now

--- chatbot.php
+++ chatbot.php
@@ -71,18 +71,5 @@ else {
   $send_result = send_reply_message($API_URL . '/reply', $POST_HEADER, $post_body);
 }
 
-# Reply Messages
-function send_reply_message($url, $post_header, $post_body)
-{
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  $result = curl_exec($ch);
-  curl_close($ch);
-  return $result;
-}
 
 
--- function.php
+++ function.php
@@ -1,4 +1,5 @@
 <?
+# Get API
 function file_get_contents_curl($url)
 {
     $ch = curl_init();
@@ -9,4 +10,18 @@ function file_get_contents_curl($url)
     $data = curl_exec($ch);
     curl_close($ch);
     return $data;
+}
+
+# Reply Messages
+function send_reply_message($url, $post_header, $post_body)
+{
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  $result = curl_exec($ch);
+  curl_close($ch);
+  return $result;
 }
