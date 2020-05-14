7163ce6b82989ef2ccb9a4ed6ac5f7094a727527
thanakrit <thanakrit.10197@gmail.com>
2020-05-14 18:01:49 +0700

    now

--- web/order_history.php
+++ web/order_history.php
@@ -37,6 +37,7 @@
                             </article>
                             <div id="received" class="col-1 p-0 m-0"></div>
                         </div>
+                        <br>
                         <div class="row p-0 m-0">
                             <div class="col-3 p-0 m-0">
                                 <img src="top.jpg" id="img-content" class="img-fluid p-0 m-0">
@@ -48,6 +49,7 @@
                             </article>
                             <div id="sending" class="col-1 p-0 m-0"></div>
                         </div>
+                        <br>
                         <div class="row p-0 m-0">
                             <div class="col-3 p-0 m-0">
                                 <img src="top.jpg" id="img-content" class="img-fluid p-0 m-0">
@@ -59,6 +61,20 @@
                             </article>
                             <div id="cancel" class="col-1 p-0 m-0"></div>
                         </div>
+                        <hr>
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination justify-content-center">
+                                <li class="page-item disabled">
+                                    <a class="page-link" href="#" tabindex="-1">Previous</a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#">Next</a>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
             </div>
