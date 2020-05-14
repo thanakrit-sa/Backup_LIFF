e8142b48ec9d1bb01f61d0c78d1b6dbbb30e24ae
thanakrit <thanakrit.10197@gmail.com>
2020-05-14 16:32:54 +0700

    now

--- web/order_history.php
+++ web/order_history.php
@@ -25,6 +25,8 @@
 ?>
 
 <body>
-    <div class="top_image p-0 m-0">
-
+    <div class="row">
+        <div class="col">
+            <img src="top.jpg" class="img-fluid p-0 m-0">
+        </div>
     </div>
