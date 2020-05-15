aa64a3b772440479f1814d50314ed33b8659257b
thanakrit <thanakrit.10197@gmail.com>
2020-05-15 18:01:20 +0700

    now

--- web/Product/all_product.php
+++ web/Product/all_product.php
@@ -15,13 +15,13 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="row">
-            <div class="col">
+            <div class="col-8">
                 <form class="form-inline my-2 my-lg-0">
                     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
-            <div class="col">
+            <div class="col-4">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
