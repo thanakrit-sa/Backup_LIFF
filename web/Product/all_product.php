30bf623e7e1768d16951a209d31379dc0f1384f0
thanakrit <thanakrit.10197@gmail.com>
2020-05-15 18:49:21 +0700

    now

--- web/Product/all_product.php
+++ web/Product/all_product.php
@@ -56,7 +56,7 @@ foreach ($dataFromApi['data'] as $data) {
             <div class="card-body p-0 m-0 scroll">
                 <div class="row p-0 m-0">
                     <div class="col-6 p-0 m-0" align="center">
-                        <a style="text-decoration:none; color:black" href="../Detail_Product/detail_1.php">
+                        <a style="text-decoration:none; color:black" href="../Product/product_1.php">
                             <div class="card mt-2 ml-2 mr-1 mb-2 p-0 shadow bg-white">
                                 <div class="card-body p-0 m-0">
                                     <div class="row p-0 m-0">
