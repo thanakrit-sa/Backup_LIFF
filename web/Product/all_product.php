3829a920b585e5e8d148cf06ceaf2a857fb3e442
thanakrit <thanakrit.10197@gmail.com>
2020-05-15 17:55:49 +0700

    now

--- web/Product/all_product.php
+++ web/Product/all_product.php
@@ -301,6 +301,10 @@
                 </div>
             </div>
         </div>
+        <button class="btn btn-primary" type="button" disabled>
+            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            Loading...
+        </button>
     </div>
 </body>
 
