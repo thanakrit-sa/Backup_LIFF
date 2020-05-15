35b02db78079787b72793cbf8ed8a59fc55dee05
thanakrit <thanakrit.10197@gmail.com>
2020-05-15 17:49:05 +0700

    now

--- web/Product/all_product.php
+++ web/Product/all_product.php
@@ -13,6 +13,41 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
     <div class="container p-0 m-0">
         <div class="card p-0 m-0 bg-light">
             <div class="card-body p-0 m-0 scroll">
@@ -264,25 +299,6 @@
                         </a>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    <div class="footer_allpage">
-        <div class="row" align="center">
-            <div class="col-4">
-                <a href="https://www.facebook.com"><button type="button" class="btn btn-dark px-3 m-0">
-                        <i class='fab fa-facebook-square' style='font-size:40px'></i>
-                    </button></a>
-            </div>
-            <div class="col-4">
-                <a href="https://www.gmail.com"><button type="button" class="btn btn-dark px-3 m-0">
-                        <i class='fas fa-envelope' style='font-size:40px'></i>
-                    </button></a>
-            </div>
-            <div class="col-4">
-                <button type="button" class="btn btn-dark px-3 m-0" data-toggle="modal" data-target="#contactModal">
-                    <i class='fas fa-address-card' style='font-size:40px'></i>
-                </button>
             </div>
         </div>
     </div>
