ca5d60f4f1f646e090dda17ee3150f66dbb5a6a4
thanakrit <thanakrit.10197@gmail.com>
2020-05-15 16:56:19 +0700

    now

--- web/Product/all_product.php
+++ web/Product/all_product.php
@@ -27,8 +27,10 @@
                                             <div class="holder_wrap_img">
                                                 <img src="https://s.isanook.com/he/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2hlLzAvdWQvMy8xODg0MS9hc3Ryb25hdXQuanBn.jpg" class="p-0 m-0" id="img-product">
 
-                                                <div class="inner_position_right">
-                                                    <div class="ribbon">
+                                                <div class="col-md-3">
+                                                    <div class="ribbon-content">
+                                                        <div class="ribbon red"><span>Limited Offer</span></div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
