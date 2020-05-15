e09e2bf96573fcf9d580c16e6e6563eedf65cb39
thanakrit <thanakrit.10197@gmail.com>
2020-05-15 14:40:03 +0700

    now

--- web/Product/all_product.php
+++ web/Product/all_product.php
@@ -22,13 +22,12 @@
                             <div class="card-body p-0 m-0">
                                 <div class="row p-0 m-0">
                                     <div class="col p-0 m-0">
-                                        <img src="https://s.isanook.com/he/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2hlLzAvdWQvMy8xODg0MS9hc3Ryb25hdXQuanBn.jpg" 
-                                        class="img-thumbnail p-0 m-0" id="img-content">
+                                        <img src="https://s.isanook.com/he/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2hlLzAvdWQvMy8xODg0MS9hc3Ryb25hdXQuanBn.jpg" class="img-thumbnail p-0 m-0" id="img-content">
                                     </div>
-                                    <article class="col p-1 m-1">
-                                        <b>ชื่อสินค้า</b> สินค้าที่ 1
-                                    </article>
                                 </div>
+                                <article class="พนไ p-1 m-1">
+                                    <b>ชื่อสินค้า</b> สินค้าที่ 1
+                                </article>
                             </div>
                         </div>
                     </div>
@@ -37,8 +36,7 @@
                             <div class="card-body p-0 m-0">
                                 <div class="row p-0 m-0">
                                     <div class="col p-0 m-0">
-                                        <img src="https://s.isanook.com/he/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2hlLzAvdWQvMy8xODg0MS9hc3Ryb25hdXQuanBn.jpg" 
-                                        class="img-thumbnail p-0 m-0" id="img-content">
+                                        <img src="https://s.isanook.com/he/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2hlLzAvdWQvMy8xODg0MS9hc3Ryb25hdXQuanBn.jpg" class="img-thumbnail p-0 m-0" id="img-content">
                                     </div>
                                 </div>
                             </div>
