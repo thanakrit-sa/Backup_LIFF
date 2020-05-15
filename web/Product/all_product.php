b63c30cbfc754d06274d7db3d2a87149b9bf488d
thanakrit <thanakrit.10197@gmail.com>
2020-05-15 18:10:18 +0700

    now

--- web/Product/all_product.php
+++ web/Product/all_product.php
@@ -24,7 +24,16 @@
                 </div>
             </div>
             <div class="col-4">
-
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        หมวดหมู่
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                        <button class="dropdown-item" type="button">Action</button>
+                        <button class="dropdown-item" type="button">Another action</button>
+                        <button class="dropdown-item" type="button">Something else here</button>
+                    </div>
+                </div>
             </div>
         </div>
     </nav>
