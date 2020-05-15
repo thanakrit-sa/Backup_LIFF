c858eb41ef42684d8fcdeebb44975bcc61046a18
thanakrit <thanakrit.10197@gmail.com>
2020-05-15 14:33:32 +0700

    now

--- web/Product/all_product.php
+++ web/Product/all_product.php
@@ -14,7 +14,7 @@
 
 <body>
     <div class="container">
-        <div class="card mt-2">
+        <div class="card p-0 mt-2">
             <div class="card-body p-0 m-0">
                 <div class="row p-0 m-0">
                     <div class="col-6 p-0 m-0" align="center">
