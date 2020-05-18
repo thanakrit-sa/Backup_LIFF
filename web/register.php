3c94bdc9fb9f8e4cdce5dcafbdb4bb498f080da7
thanakrit <thanakrit.10197@gmail.com>
2020-05-18 12:01:33 +0700

    now

--- web/register.php
+++ web/register.php
@@ -22,17 +22,17 @@ $data = array(
 ); //ข้อมูลที่ต้องการโพส
 $data_string = json_encode($data); //ทำให้เป็น json ด้วยฟังก์ชัน json_encode
  echo $data_string;
-// $ch = curl_init('https://e-sport.in.th/ssdev/ecom/dashboard/api/member/register'); //ตรงนี้อย่าลืมเปลี่ยนเป็น url ที่ต้องการส่งค่าไปนะครับ
+$ch = curl_init('https://e-sport.in.th/ssdev/ecom/dashboard/api/member/register'); //ตรงนี้อย่าลืมเปลี่ยนเป็น url ที่ต้องการส่งค่าไปนะครับ
  
-// //ในกรณีที่ต้องการส่งเป็น method อื่น เช่น DELETE, PUT, HEAD ก็เปลี่ยนได้ที่นี่ครับ
-// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+//ในกรณีที่ต้องการส่งเป็น method อื่น เช่น DELETE, PUT, HEAD ก็เปลี่ยนได้ที่นี่ครับ
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
  
-// curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-//     'Content-Type: application/json', //ระบุว่าค่าที่ส่งไปเป็นแบบ json
-//     'Content-Length: ' . strlen($data_string))  //บอกขนาดของ json ที่ส่งไปด้วย
-// );
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json', //ระบุว่าค่าที่ส่งไปเป็นแบบ json
+    'Content-Length: ' . strlen($data_string))  //บอกขนาดของ json ที่ส่งไปด้วย
+);
  
-// $result = curl_exec($ch);
-// curl_close($ch);
+$result = curl_exec($ch);
+curl_close($ch);
