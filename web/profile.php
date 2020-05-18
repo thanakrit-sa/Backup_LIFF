669db5ed24ce9fc93ee901e8a4c122a752a21238
thanakrit <thanakrit.10197@gmail.com>
2020-05-18 14:06:56 +0700

    now

--- web/profile.php
+++ web/profile.php
@@ -11,7 +11,6 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="style_profile.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <script src="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
 </head>
 <script src="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
   <script>
