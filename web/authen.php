66ca68346ebd02346ecf78cdf257dfd7be2b93e5
thanakrit <thanakrit.10197@gmail.com>
2020-05-18 14:24:19 +0700

    now

--- web/authen.php
+++ web/authen.php
@@ -4,16 +4,15 @@
         "line_userid" => "$lineID",
     );
     $data_string = json_encode($data);
-    echo $data_string;
     
-    
-    // $ch = curl_init('https://e-sport.in.th/ssdev/ecom/dashboard/api/member/register');
+    $ch = curl_init('https://e-sport.in.th/ssdev/ecom/dashboard/api/member/linelogin');
      
-    // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    // curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',));
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',));
      
-    // $result = curl_exec($ch);
-    // curl_close($ch);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    echo $result;
 ?>
