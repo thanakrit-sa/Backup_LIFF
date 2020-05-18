266529b981efde9da3aa3fcc36af216bf3ee3f48
thanakrit <thanakrit.10197@gmail.com>
2020-05-18 10:56:56 +0700

    now

---
+++ web/login.php
@@ -0,0 +1,8 @@
+<?
+ 
+?>
+
+<form class="form-group" action="" method="POST">
+    <input class="form-control" type="text" name="username" placeholder="Username">
+    <input class="form-control" type="password" name="password" placeholder="Password">
+</form>
