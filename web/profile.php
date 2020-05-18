6a4a0444a292143ac5efc71f46eb38c40260771e
thanakrit <thanakrit.10197@gmail.com>
2020-05-18 17:08:43 +0700

    now

--- web/profile.php
+++ web/profile.php
@@ -107,16 +107,16 @@
 </div>
 </div> -->
 <?
+// $lineID = $_GET['lineID'];
+// $data = array(
+//     "line_userid" => "$lineID"
+// );
+// $data_string = json_encode($data);
 $lineID = $_GET['lineID'];
-$data = array(
-    "line_userid" => "$lineID"
-);
-$data_string = json_encode($data);
 
-$ch = curl_init('https://e-sport.in.th/ssdev/ecom/dashboard/api/member/memberlineid/line_userid');
+$ch = curl_init('https://e-sport.in.th/ssdev/ecom/dashboard/api/member/memberlineid/'.$lineID);
 
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',));
 
