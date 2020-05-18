a0e5d6a265a9ad8c480139f0f60d36399cb47d2a
thanakrit <thanakrit.10197@gmail.com>
2020-05-18 14:43:01 +0700

    now

--- web/check.php
+++ web/check.php
@@ -1,24 +1,24 @@
 <?
-    $lineID = $_GET['lineID'];
-    $data = array(
-        "line_userid" => "$lineID",
-    );
-    $data_string = json_encode($data);
-    
-    $ch = curl_init('https://e-sport.in.th/ssdev/ecom/dashboard/api/member/linelogin');
-     
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',));
-     
-    $result = curl_exec($ch);
-    curl_close($ch);
-    $data = json_decode($result, true);
+$lineID = $_GET['lineID'];
+$data = array(
+    "line_userid" => "$lineID",
+);
+$data_string = json_encode($data);
 
-    if($data["resultMgs"] == "Unsuccessfully") {
-        echo '<A HREF = "http://www.thaiseoboard.com "> Thaiseoboard </A>';
-    }
-    else {
-        echo "not";
-    }
+$ch = curl_init('https://e-sport.in.th/ssdev/ecom/dashboard/api/member/linelogin');
+
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',));
+
+$result = curl_exec($ch);
+curl_close($ch);
+$data = json_decode($result, true);
+
+if ($data["resultMgs"] == "Unsuccessfully") {
+    header("Location: register.php");
+    exit;
+} else {
+    echo "not";
+}
