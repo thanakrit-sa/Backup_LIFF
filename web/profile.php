a4f6f82d64064d387be70614eeda786ea3eb2846
thanakrit <thanakrit.10197@gmail.com>
2020-05-18 17:30:48 +0700

    now

--- web/profile.php
+++ web/profile.php
@@ -45,7 +45,7 @@ $updated = $data['updated_at'];
                 <div class="card-body">
                     <div class="row">
                         <div class="col" align="center">
-                            <img src="images/catdog.jpg" class="thumbnail rounded-circle shadow p-0 m-0 bg-black rounded" width="50%">
+                            <img src="Images/catdog.jpg" class="thumbnail rounded-circle shadow p-0 m-0 bg-black rounded">
                         </div>
                     </div>
                     <br>
