5d3c4ab0c8ef5ee92e95d4b2ac6570c495abc6a2
thanakrit <thanakrit.10197@gmail.com>
2020-06-29 15:36:14 +0700

    now

--- chatbot.php
+++ chatbot.php
@@ -1,4 +1,29 @@
 <?php
+function file_get_contents_curl($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+
+# Reply Messages
+function send_reply_message($url, $post_header, $post_body)
+{
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  $result = curl_exec($ch);
+  curl_close($ch);
+  return $result;
+}
 
 $API_URL = 'https://api.line.me/v2/bot/message';
 $ACCESS_TOKEN = 'Es3Kz8W5FIyX+e9W8QhhNvTreG4FuPaUwlTi/CCK5+g51055N5mYYzPLtcFOEfe3Mrdtvk0KNvGP3owBpYOBIE/Xq3aDuJ+w0VI/3Eelkl7/bvEz+Kv2K0pBsumqTnDpQDXTqsC7yucteBdhejsnXwdB04t89/1O/w1cDnyilFU=';
@@ -12,7 +37,6 @@ foreach ($request_array['events'] as $event) {
   $reply_token = $event['replyToken'];
 }
 
-include 'function.php';
 
 // $html = file_get_contents_curl("http://dummy.restapiexample.com/api/v1/employees"); #API Dummy
 // $dataFromApi = json_decode($html, true);
