f6f718f4ea342837b2ca9fde33889ec1d3247b8f
thanakrit <thanakrit.10197@gmail.com>
2020-06-30 16:42:22 +0700

    now

--- flex_message.php
+++ flex_message.php
@@ -534,7 +534,7 @@ $prod_fasionMen = [
           "type"=> "bubble",
           "hero"=> [
             "type"=> "image",
-            "url"=> "https://my-live-02.slatic.net/original/544da1eaf8adf5cc082eedabaa917019.jpg",
+            "url"=> $prod_image[0],
             "size"=> "full",
             "aspectRatio"=> "1:1",
             "aspectMode"=> "cover",
@@ -571,7 +571,7 @@ $prod_fasionMen = [
                     "contents"=> [
                       [
                         "type"=> "text",
-                        "text"=> "฿569.00",
+                        "text"=> "฿ ".$prod_price[0],
                         "flex"=> 0,
                         "size"=> "lg",
                         "weight"=> "bold",
@@ -586,7 +586,7 @@ $prod_fasionMen = [
                     "contents"=> [
                       [
                         "type"=> "text",
-                        "text"=> "เหลือเพียง 5 ชิ้นเท่านั้น",
+                        "text"=> "เหลือเพียง ".$prod_stock[0]." ชิ้นเท่านั้น",
                         "size"=> "sm",
                         "weight"=> "bold"
                     ]
