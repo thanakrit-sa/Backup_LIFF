390e0878ed4eefe35bfeedb944f6b2a46a0732bc
thanakrit <thanakrit.10197@gmail.com>
2020-07-01 15:36:03 +0700

    now

--- flex_message.php
+++ flex_message.php
@@ -606,7 +606,7 @@ $prod_Recommend = [
             "action"=> [
                 "type"=> "message",
                 "label"=> "สั่งซื้อ",
-                "text"=> "recommend26"
+                "text"=> "recommend_".$split_prod
              ],
             "color"=> "#2E5A7D",
             "style"=> "primary"
