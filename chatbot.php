b7d02fafb54e0970d7d78add9ebab8e774321247
thanakrit <thanakrit.10197@gmail.com>
2020-07-01 16:10:46 +0700

    now

--- chatbot.php
+++ chatbot.php
@@ -59,17 +59,17 @@ if ($message == "แสดงสินค้า") {
 } else if (strpos($message, "_")) {
   $splie = explode("_", $message);
   $split_word = $split[1];
-  if ($split_word = "261") {
-    $data = [
-      'replyToken' => $reply_token,
-      'messages' => [$recommend]
-    ];
-  } else {
-    $data = [
-      'replyToken' => $reply_token,
-      'messages' => [$recommend1]
-    ];
-  }
+  // if ($split_word = "261") {
+  //   $data = [
+  //     'replyToken' => $reply_token,
+  //     'messages' => [$recommend]
+  //   ];
+  // } else {
+  //   $data = [
+  //     'replyToken' => $reply_token,
+  //     'messages' => [$recommend1]
+  //   ];
+  // }
 } else {
   $data = [
     'replyToken' => $reply_token,
