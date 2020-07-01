f7792b27ea0bfb3ab2e2b0e4ec24c59e974fc018
thanakrit <thanakrit.10197@gmail.com>
2020-07-01 15:25:44 +0700

    now

--- chatbot.php
+++ chatbot.php
@@ -60,15 +60,8 @@ if ($message == "แสดงสินค้า") {
   ];
   $post_body = json_encode($data, true);
   $send_result = send_reply_message($API_URL . '/reply', $POST_HEADER, $post_body);
-} else if ($message == "recommend_25") {
-  $api = file_get_contents_curl("https://e-sport.in.th/ssdev/ecom/dashboard/api/products/productBycat/$split_prod");
-  $dataFromApi = json_decode($api, true);
-  foreach ($dataFromApi['data'] as $data) {
-    $recommend_name[] = $data['product_name'];
-    $recommend_image[] = $data['image_path'];
-    $recommend_stock[] = $data['stock'];
-    $recommend_price[] = $data['price'];
-  }
+} else if (strpos($message,"recommend") == true) {
+  
   $data = [
     'replyToken' => $reply_token,
     'messages' => [$recommend]
