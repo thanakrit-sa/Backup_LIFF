5b5161ad7f72ddc4148661bc7c15f9641680137b
thanakrit <thanakrit.10197@gmail.com>
2020-07-01 18:06:59 +0700

    now

--- flex_message.php
+++ flex_message.php
@@ -851,7 +851,7 @@ $recommend_1 = [
             "action"=> [
               "type"=> "uri",
               "label"=> "สั่งซื้อ",
-              "uri"=> "https://liff.line.me/1654173341-mW8WAoN5/path_A/?key1=value1#URL-fragment"
+              "uri"=> "https://liff.line.me/1654173341-mW8WAoN5/path_A/?key=v11#URL-fragment"
             ],
             "color"=> "#2E5A7D",
             "style"=> "primary"
