e16945b1c9aa31dce1645af84cddb1c74e33af0a
thanakrit <thanakrit.10197@gmail.com>
2020-07-02 21:12:04 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -9,6 +9,7 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="../style_profile.css">
 </head>
 
 <body>
@@ -40,9 +41,9 @@
                 <h6 class="m-0"><?= $recommend_name[$split[0] - 1] ?></h6>
                 <h4><strong class="m-0">฿<?= $recommend_price[$split[0] - 1] ?></strong></h4>
             </div>
-            <button type="button" class="btn btn-lg btn-dark rounded-circle">+</button>
+            <button type="button" class="btn btn-lg btn-dark rounded-circle" style="width:50px; height:50px;">+</button>
             <button type="button" disabled class="btn btn-lg bg-white mx-3 px-3">1</button>
-            <button type="button" class="btn btn-lg btn-dark rounded-circle">- </button>
+            <button type="button" class="btn btn-lg btn-dark rounded-circle" style="width:50px; height:50px;">- </button>
         </li>
     </ul>
 </body>
