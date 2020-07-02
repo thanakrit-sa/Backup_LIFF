735d71ed6b4642321e7bc3e86cbb25efc49af6c1
thanakrit <thanakrit.10197@gmail.com>
2020-07-02 11:29:08 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -8,23 +8,25 @@
 </head>
 <body>
 <?
+    include '../../function.php'
     $message = $_GET['message'];
     $split = explode(":", $message);
     $split_prod = $split[2];
-    // $api = file_get_contents_curl("https://e-sport.in.th/ssdev/ecom/dashboard/api/products/productBycat/$split_prod");
-    // $dataFromApi = json_decode($api, true);
-    // foreach ($dataFromApi['data'] as $data) {
-    //     $recommend_name[] = $data['product_name'];
-    //     $recommend_image[] = $data['image_path'];
-    //     $recommend_stock[] = $data['stock'];
-    //     $recommend_price[] = $data['price'];
-    // }
+    $api = file_get_contents_curl("https://e-sport.in.th/ssdev/ecom/dashboard/api/products/productBycat/$split_prod");
+    $dataFromApi = json_decode($api, true);
+    foreach ($dataFromApi['data'] as $data) {
+        $recommend_name[] = $data['product_name'];
+        $recommend_image[] = $data['image_path'];
+        $recommend_stock[] = $data['stock'];
+        $recommend_price[] = $data['price'];
+    }
 ?>
     <h2>hello</h2>
     <p><?= $message ?></p>
     <p><?= $split[0] ?></p>
     <p><?= $split[1] ?></p>
     <p><?= $split[2] ?></p>
+    <p><?= $recommend_name[0] ?></p>
 </body>
 
 </html>
