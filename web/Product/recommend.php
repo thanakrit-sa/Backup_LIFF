597b6b4a89f35b4a3ff1959868faee0110df2736
thanakrit <thanakrit.10197@gmail.com>
2020-07-02 11:26:29 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -6,26 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
 <?
     $message = $_GET['message'];
     $split = explode(":", $message);
     $split_prod = $split[2];
-    $api = file_get_contents_curl("https://e-sport.in.th/ssdev/ecom/dashboard/api/products/productBycat/$split_prod");
-    $dataFromApi = json_decode($api, true);
-    foreach ($dataFromApi['data'] as $data) {
-        $recommend_name[] = $data['product_name'];
-        $recommend_image[] = $data['image_path'];
-        $recommend_stock[] = $data['stock'];
-        $recommend_price[] = $data['price'];
-    }
+    // $api = file_get_contents_curl("https://e-sport.in.th/ssdev/ecom/dashboard/api/products/productBycat/$split_prod");
+    // $dataFromApi = json_decode($api, true);
+    // foreach ($dataFromApi['data'] as $data) {
+    //     $recommend_name[] = $data['product_name'];
+    //     $recommend_image[] = $data['image_path'];
+    //     $recommend_stock[] = $data['stock'];
+    //     $recommend_price[] = $data['price'];
+    // }
 ?>
     <h2>hello</h2>
-    <p><?= $recommend_name[0] ?></p>
-    <p><?= $recommend_image[0] ?></p>
-    <p><?= $recommend_stock[0] ?></p>
-    <p><?= $recommend_price[0] ?></p>
+    <p><?= $message ?></p>
 </body>
 
 </html>
