26aa0a84b0856b376b45c0aa327a875deb36a252
thanakrit <thanakrit.10197@gmail.com>
2020-07-02 22:32:40 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -72,6 +72,7 @@
             </div>
         </li>
     </ul>
+    <br>
     <div class="footer">
         <div class="row">
             <div class="col-2 m-1 p-0">
@@ -80,7 +81,7 @@
             <div class="col-2 m-1 p-0">
                 <button class="btn btn-lg btn-block btn-success">ซื้อ</button>
             </div>
-            <div class="col-7 m-1 p-0">
+            <div class="col-8 ml-1 p-0">
                 <button class="btn btn-lg btn-block btn-success">ซื้อเลย</button>
             </div>
         </div>
