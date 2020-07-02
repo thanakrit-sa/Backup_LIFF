472cd4455aba1b5085b1631bc3b5721548beda04
thanakrit <thanakrit.10197@gmail.com>
2020-07-02 11:33:17 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -14,11 +14,25 @@
     $split_prod = $split[2];
     $api = file_get_contents_curl("https://e-sport.in.th/ssdev/ecom/dashboard/api/products/productBycat/$split_prod");
     $dataFromApi = json_decode($api, true);
-    foreach ($dataFromApi['data'] as $data) {
-        $recommend_name[] = $data['product_name'];
-        $recommend_image[] = $data['image_path'];
-        $recommend_stock[] = $data['stock'];
-        $recommend_price[] = $data['price'];
+    
+    function a ($key) {
+        foreach ($dataFromApi['data'] as $data) {
+            $recommend_name[$key] = $data['product_name'];
+            $recommend_image[$key] = $data['image_path'];
+            $recommend_stock[$key] = $data['stock'];
+            $recommend_price[$key] = $data['price'];
+        }
+    }
+
+    if ($split[0] == "1") {
+        $key = 0;
+        a($key);
+    } else if ($split[0] == "2") {
+
+    } else if ($split[0] == "3") {
+
+    } else {
+        echo "Error";
     }
 ?>
     <h2>hello</h2>
@@ -26,7 +40,7 @@
     <p><?= $split[0] ?></p>
     <p><?= $split[1] ?></p>
     <p><?= $split[2] ?></p>
-    <p><?= $recommend_name[0] ?></p>
+    <p><?= $recommend_name[$key] ?></p>
 </body>
 
 </html>
