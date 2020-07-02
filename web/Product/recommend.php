4aa1046c76c55d1f51597d9aaf5023578fdaccaf
thanakrit <thanakrit.10197@gmail.com>
2020-07-02 22:35:33 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -78,10 +78,10 @@
             <div class="col-2 mr-1 p-0">
                 <button class="btn btn-lg btn-block btn-success">ซื้อ</button>
             </div>
-            <div class="col-2 mr-1 p-0">
+            <div class="col-2 m-0 p-0">
                 <button class="btn btn-lg btn-block btn-success">ซื้อ</button>
             </div>
-            <div class="col-8 m-0 p-0">
+            <div class="col-8 ml-1 p-0">
                 <button class="btn btn-lg btn-block btn-success">ซื้อเลย</button>
             </div>
         </div>
