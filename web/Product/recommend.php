001049c7a794d5e0cab35801ffc5b72a9785a97b
thanakrit <thanakrit.10197@gmail.com>
2020-07-02 21:20:16 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -38,7 +38,8 @@
         </li>
         <li class="list-group-item m-1">
             <div class="mb-3">
-                <h6 class="mb-1"><?= $recommend_name[$split[0] - 1] ?></h6>
+                <h5 class="mb-1"><?= $recommend_name[$split[0] - 1] ?></h5>
+                <br>
                 <h4><strong class="m-0">฿<?= $recommend_price[$split[0] - 1] ?></strong></h4>
             </div>
             <button type="button" class="btn btn-dark rounded-circle" style="width:50px; height:50px;">+</button>
