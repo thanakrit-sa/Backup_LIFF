1a2d4bf1dacba557a9a4deee07764f4fa08cf3b2
thanakrit <thanakrit.10197@gmail.com>
2020-07-02 21:16:52 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -38,12 +38,12 @@
         </li>
         <li class="list-group-item m-1">
             <div class="mb-3">
-                <h6 class="m-0"><?= $recommend_name[$split[0] - 1] ?></h6>
-                <h4><strong class="mt-1">฿<?= $recommend_price[$split[0] - 1] ?></strong></h4>
+                <h6 class="mb-1"><?= $recommend_name[$split[0] - 1] ?></h6>
+                <h4><strong class="m-0">฿<?= $recommend_price[$split[0] - 1] ?></strong></h4>
             </div>
-            <button type="button" class="btn btn-dark rounded-circle" style="width:50px; height:50px;">+</button>
+            <button type="button" class="btn btn-white border border-secondary rounded-circle" style="width:30px; height:30px;">+</button>
             <button type="button" disabled class="btn btn-lg bg-white mx-3 px-3">1</button>
-            <button type="button" class="btn btn-dark rounded-circle" style="width:50px; height:50px;">- </button>
+            <button type="button" class="btn btn-white border border-secondary rounded-circle" style="width:30px; height:30px;">- </button>
         </li>
     </ul>
 </body>
