9f5badf56315156ecb69a8e106b304fdc53a8bfb
thanakrit <thanakrit.10197@gmail.com>
2020-07-02 11:36:45 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -6,27 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<?
+    <?
     include '../../function.php';
     $message = $_GET['message'];
     $split = explode(":", $message);
     $split_prod = $split[2];
     $api = file_get_contents_curl("https://e-sport.in.th/ssdev/ecom/dashboard/api/products/productBycat/$split_prod");
     $dataFromApi = json_decode($api, true);
-    
-    function a ($key) {
-        foreach ($dataFromApi['data'] as $data) {
-            $recommend_name[$key] = $data['product_name'];
-            $recommend_image[$key] = $data['image_path'];
-            $recommend_stock[$key] = $data['stock'];
-            $recommend_price[$key] = $data['price'];
-        }
-    }
-
+   
     if ($split[0] == "1") {
-        $key = 0;
-        a($key);
+        foreach ($dataFromApi['data'] as $data) {
+            $recommend_name[$split[0]] = $data['product_name'];
+            $recommend_image[$split[0]] = $data['image_path'];
+            $recommend_stock[$split[0]] = $data['stock'];
+            $recommend_price[$split[0]] = $data['price'];
+        }
     } else if ($split[0] == "2") {
 
     } else if ($split[0] == "3") {
@@ -40,7 +36,7 @@
     <p><?= $split[0] ?></p>
     <p><?= $split[1] ?></p>
     <p><?= $split[2] ?></p>
-    <p><?= $recommend_name[$key] ?></p>
+    <p><?= $recommend_name[$split[0]] ?></p>
 </body>
 
 </html>
