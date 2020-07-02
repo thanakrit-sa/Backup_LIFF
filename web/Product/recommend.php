c09e7dab8ac74e0f18431961f4896b4f272a7586
thanakrit <thanakrit.10197@gmail.com>
2020-07-02 11:51:45 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -17,10 +17,10 @@
     $api = file_get_contents_curl("https://e-sport.in.th/ssdev/ecom/dashboard/api/products/productBycat/$split_prod");
     $dataFromApi = json_decode($api, true);
     foreach ($dataFromApi['data'] as $data) {
-        $recommend_name[$split_p] = $data['product_name'];
-        $recommend_image[$split[0]] = $data['image_path'];
-        $recommend_stock[$split[0]] = $data['stock'];
-        $recommend_price[$split[0]] = $data['price'];
+        $recommend_name[] = $data['product_name'];
+        $recommend_image[] = $data['image_path'];
+        $recommend_stock[] = $data['stock'];
+        $recommend_price[] = $data['price'];
     }
 
 ?>
