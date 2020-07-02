8c83e1ebb96145f0dd813192a4ae2ec62c0552c7
thanakrit <thanakrit.10197@gmail.com>
2020-07-02 12:23:45 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -28,7 +28,7 @@
     $img_url = "https://e-sport.in.th/ssdev/ecom/dashboard/uploads/img_prod/".$recommend_image[$split[0]-1];
 
 ?>
-    <div align="center" class="shadow">
+    <div align="center" class="shadow m-0">
         <p class="p-3"><?= $recommend_name[$split[0] - 1] ?></p>
     </div>
     <img src=<?=$img_url?> class="img-fluid m-0">
