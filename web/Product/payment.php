2c992541329c7ce8f1956b86727f8b4ded5635ac
thanakrit <thanakrit.10197@gmail.com>
2020-07-03 16:54:42 +0700

    now

--- web/Product/payment.php
+++ web/Product/payment.php
@@ -21,11 +21,11 @@
         $api = file_get_contents_curl("https://e-sport.in.th/ssdev/ecom/dashboard/api/products/productByid/20");
         $dataFromApi = json_decode($api, true);
         foreach ($dataFromApi['data'] as $data) {
-            $recommend_id[] = $data['id'];
-            $recommend_name[] = $data['product_name'];
-            $recommend_image[] = $data['image_path'];
-            $recommend_stock[] = $data['stock'];
-            $recommend_price[] = $data['price'];
+            $recommend_id = $data['id'];
+            $recommend_name = $data['product_name'];
+            $recommend_image = $data['image_path'];
+            $recommend_stock = $data['stock'];
+            $recommend_price = $data['price'];
         }
     ?>
     <h3><?= $quantity ?></h3>
