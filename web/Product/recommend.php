5a8b108ca848099ed4dc3773cb9403da029e9823
thanakrit <thanakrit.10197@gmail.com>
2020-07-03 11:21:39 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -77,7 +77,7 @@
                 <br class="m-0 p-0">
                 <!-- <label disabled class="btn btn-lg bg-white mx-1 mt-2">1</label> -->
                 <form name="form1">
-                    <button type="button" onclick="Myfunction()" class="btn btn-dark rounded-circle" style="width:40px; height:40px;">+</button>
+                    <button type="button" onclick=<?Myfunction()?> class="btn btn-dark rounded-circle" style="width:40px; height:40px;">+</button>
                     <input type="text" name="input1" value="<?php echo $value;?>" size="1" style="border: 0; text-align: center;">
                     <button type="button" class="btn btn-dark rounded-circle" style="width:40px; height:40px;">- </button>
                 </form>
