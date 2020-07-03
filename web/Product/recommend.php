9021d1920d8c172e9e4051ee9bfc0afae8098759
thanakrit <thanakrit.10197@gmail.com>
2020-07-03 12:40:38 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -77,13 +77,14 @@
                     <script>
                         function handle() {
                             var a = form1.input1.value++
+                            <?=$n?> = a+1
                             console.log(a+1);
                         }
                     </script>
                     <button type="button" onclick="javascript:handle()" class="btn btn-dark rounded-circle" style="width:40px; height:40px;">+</button>
                     <input readonly type="text" name="input1" value="<?= $n ?>" onchange="javascript:handle()" size="1" style="border: 0; text-align: center;">
                     <?= $n ?>
-                    <button type="button" disabled="<?if ($n == " 0") { true; } else { false; };?>" onclick="javascript:form1.input1.value--" class="btn btn-dark rounded-circle" style="width:40px; height:40px;">- </button>
+                    <!-- <button type="button" disabled="<?if ($n == "0") { true; } else { false; };?>" onclick="javascript:form1.input1.value--" class="btn btn-dark rounded-circle" style="width:40px; height:40px;">- </button> -->
                 </form>
             </div>
         </li>
