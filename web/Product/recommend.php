99369048ae03f81887e3121072f7f6054279f81c
thanakrit <thanakrit.10197@gmail.com>
2020-07-03 16:20:15 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -97,10 +97,10 @@
                             if (stock == "0") {
                                 document.formData.buttonDown.disabled = true;
                                 document.formData.buttonUp.disabled = false;
-                                // document.formData.stock.value--
+                                document.formData.stock.value--
                             } else {
                                 document.formData.buttonUp.disabled = false;
-                                // document.formData.stock.value--
+                                document.formData.stock.value--
                             }
                         }
                     </script>
