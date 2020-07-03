30d33fabe4bcaac5c0c2772d067e9207fccfa01f
thanakrit <thanakrit.10197@gmail.com>
2020-07-03 13:27:15 +0700

    now

--- web/Product/recommend.php
+++ web/Product/recommend.php
@@ -78,7 +78,7 @@
                         function handle() {
                             var a = form1.input1.value
                             console.log(a+1);
-                            if (a != "0") {
+                            if (a+1 != "0") {
                                 document.form1.button.disabled = false;
                                 document.form1.input1.value++
                             } else {
