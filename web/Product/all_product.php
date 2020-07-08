00cb090bbf2e1b877727c17e17492c0ae1a91e60
thanakrit <thanakrit.10197@gmail.com>
2020-07-08 22:40:29 +0700

    now

--- web/Product/all_product.php
+++ web/Product/all_product.php
@@ -26,7 +26,7 @@ foreach ($dataFromApi['data'] as $data) {
 ?>
 
 <body>
-    <div class="card crad-body">
+    <div class="card card-body">
         <div class="row" align="center">
             <div class="col-7 m-0 p-0">
                 <input type="text" class="form-control" placeholder="ชื่อสินค้า">
