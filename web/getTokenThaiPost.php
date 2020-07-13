8e0b6eb67dd9bdb3ddd81a6a9e562c6768c8a32e
Thanakrit Sahachatchai <thanakrit.10197@gmail.com>
2020-07-13 22:27:40 +0700

    now

--- web/getTokenThaiPost.php
+++ web/getTokenThaiPost.php
@@ -1,7 +1,7 @@
 <?
 
 
-$ch = curl_init('https://trackapi.thailandpost.co.th/post/api/v1/track');
+$ch = curl_init('https://trackapi.thailandpost.co.th/post/api/v1/authenticate/token');
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
