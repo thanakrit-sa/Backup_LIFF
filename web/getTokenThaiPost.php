323bd59d9f2119f550774139d95a446fde917c22
Thanakrit Sahachatchai <thanakrit.10197@gmail.com>
2020-07-13 22:17:52 +0700

    now

--- web/getTokenThaiPost.php
+++ web/getTokenThaiPost.php
@@ -4,7 +4,10 @@
 $ch = curl_init('https://track.thailandpost.co.th/?trackNumber=EE993673545CN');
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',));
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                    'Content-Type: application/json',
+                    'Authorization: Token HLH|N&UiJMA0FXE^KoNpRuZ!S;RwCFD+N|UNM\TxPxA6I8OxH/HIUnNCD+QqUIXzI&IdR-ObLlJ&V#E^IgNANdP?E9IxEIWeA=EZ '
+                ));
                 $result = curl_exec($ch);
                 curl_close($ch);
                 $resultData = json_decode($result, true);
