ae93bb9cbd97352ab7cddd5169ea0a2ec69cc183
Thanakrit Sahachatchai <thanakrit.10197@gmail.com>
2020-07-13 21:20:04 +0700

    now

--- web/getTokenThaiPost.php
+++ web/getTokenThaiPost.php
@@ -3,4 +3,5 @@ $headers = getallheaders(); // ได้ค่า array ของส่วน He
 // ทดสอบเก็บส่วนของข้อมูลลงในไฟล์ เพื่อดูว่า มีการส่งค่ากลับมาหรือไม่ และส่งค่าอะไรกลับมา
 file_put_contents('headers.txt',json_encode($headers, JSON_PRETTY_PRINT)); // ส่วนสำหรับใช้ตรวจสอบเพิ่มเติม
 file_put_contents('body.txt',file_get_contents('php://input')); // ส่วนของ body เป็นส่วนที่จะใช้งานถ้ามีข้อมูล
+echo file_get_contents('php://input');
 ?>
