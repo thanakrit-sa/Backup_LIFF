1838f250e21ef824a96af717a4b2b8dc98b37d16
Thanakrit Sahachatchai <thanakrit.10197@gmail.com>
2020-09-28 15:55:50 +0700

    now

--- chatbot.php
+++ chatbot.php
@@ -8,22 +8,19 @@ $replyToken = $deCode['events'][0]['replyToken'];
 $userId = $deCode['events'][0]['source']['userId'];
 $text = $deCode['events'][0]['message']['text'];
 
+$age = array("type"=>"text", "text"=>"aaa");
+
+$dataaa = json_encode($age);
+
 $messages = [];
 $messages['replyToken'] = $replyToken;
 $messages['messages'][0] = [
-  [
-    "type" => "text",
-    "text" => "Hello, world1"
-  ],
-  [
-    "type" => "text",
-    "text" => "Hello, world1"
-  ]
+  $dataaa
 ];
 
 $encodeJson = json_encode($messages);
 
-$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
+$LINEDatas['url'] = "https://api.line.me/v2/bot/message/push";
 $LINEDatas['token'] = "Es3Kz8W5FIyX+e9W8QhhNvTreG4FuPaUwlTi/CCK5+g51055N5mYYzPLtcFOEfe3Mrdtvk0KNvGP3owBpYOBIE/Xq3aDuJ+w0VI/3Eelkl7/bvEz+Kv2K0pBsumqTnDpQDXTqsC7yucteBdhejsnXwdB04t89/1O/w1cDnyilFU=";
 
 $results = sentMessage($encodeJson, $LINEDatas);
