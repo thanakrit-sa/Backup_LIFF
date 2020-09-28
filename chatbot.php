d891fca383d2d5a91643f63234872660d1b76675
Thanakrit Sahachatchai <thanakrit.10197@gmail.com>
2020-09-28 15:47:12 +0700

    now

--- chatbot.php
+++ chatbot.php
@@ -9,16 +9,20 @@ $id = $deCode['events'][0]['source']['userId'];
 $userId = $deCode['events'][0]['source']['userId'];
 $text = $deCode['events'][0]['message']['text'];
 
+$myObj->type = "text";
+$myObj->text = "aaaa";
+
+$myJSON = json_encode($myObj);
+
 $messages = [];
-// $messages['to'] = $id;
+$messages['to'] = $id;
 $messages['messages'][0] = [
-  "type" => "text",
-  "text" => "Hello, world1"
+  $myObj
 ];
 
 $encodeJson = json_encode($messages);
 
-$LINEDatas['url'] = "https://api.line.me/v2/bot/message/broadcast";
+$LINEDatas['url'] = "https://api.line.me/v2/bot/message/push";
 $LINEDatas['token'] = "Es3Kz8W5FIyX+e9W8QhhNvTreG4FuPaUwlTi/CCK5+g51055N5mYYzPLtcFOEfe3Mrdtvk0KNvGP3owBpYOBIE/Xq3aDuJ+w0VI/3Eelkl7/bvEz+Kv2K0pBsumqTnDpQDXTqsC7yucteBdhejsnXwdB04t89/1O/w1cDnyilFU=";
 
 $results = sentMessage($encodeJson, $LINEDatas);
