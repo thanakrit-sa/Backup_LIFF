a012521f219741e3fe05a73a8803cf5dfeacd765
Thanakrit Sahachatchai <thanakrit.10197@gmail.com>
2020-09-28 10:22:59 +0700

    now

--- chatbot.php
+++ chatbot.php
@@ -1,55 +1,76 @@
+<?php 
+	/*Get Data From POST Http Request*/
+	$datas = file_get_contents('php://input');
+	/*Decode Json From LINE Data Body*/
+	$deCode = json_decode($datas,true);
 
-<?php
-function file_get_contents($url)
-{
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_HEADER, 0);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-  $data = curl_exec($ch);
-  curl_close($ch);
-  return $data;
-  echo $data;
-}
+	file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
 
-function send_reply_message($url, $post_header, $post_body)
-{
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  $result = curl_exec($ch);
-  curl_close($ch);
-  return $result;
-}
+	$replyToken = $deCode['events'][0]['replyToken'];
+	$userId = $deCode['events'][0]['source']['userId'];
+	$text = $deCode['events'][0]['message']['text'];
 
-$API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'Es3Kz8W5FIyX+e9W8QhhNvTreG4FuPaUwlTi/CCK5+g51055N5mYYzPLtcFOEfe3Mrdtvk0KNvGP3owBpYOBIE/Xq3aDuJ+w0VI/3Eelkl7/bvEz+Kv2K0pBsumqTnDpQDXTqsC7yucteBdhejsnXwdB04t89/1O/w1cDnyilFU=';
-$channelSecret = 'a35820614034732a864c1e03c76bb327';
-$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
-$request = file_get_contents('php://input');
-$request_array = json_decode($request, true);
-$message = $request_array['events'][0]['message']['text'];
-foreach ($request_array['events'] as $event) {
-  $reply_token = $event['replyToken'];
-}
-$split = explode(':', $message);
-$split_prod = $split[0];
-$n = 1;
+	$messages = [];
+	$messages['replyToken'] = $replyToken;
+	$messages['messages'][0] = getFormatTextMessage("เอ้ย ถามอะไรก็ตอบได้");
 
-if ($message == "แสดงสินค้า") {
-  $data = [
-    'replyToken' => $reply_token,
-    'message' => $message
-  ];
-} else {
-  $data = [
-    'replyToken' => $reply_token,
-    'messages' => $message
-  ];
-}
-$post_body = json_encode($data, true);
-$send_result = send_reply_message($API_URL . '/reply', $POST_HEADER, $post_body);
+	$encodeJson = json_encode($messages);
+
+	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
+  	$LINEDatas['token'] = "Es3Kz8W5FIyX+e9W8QhhNvTreG4FuPaUwlTi/CCK5+g51055N5mYYzPLtcFOEfe3Mrdtvk0KNvGP3owBpYOBIE/Xq3aDuJ+w0VI/3Eelkl7/bvEz+Kv2K0pBsumqTnDpQDXTqsC7yucteBdhejsnXwdB04t89/1O/w1cDnyilFU=";
+
+  	$results = sentMessage($encodeJson,$LINEDatas);
+
+	/*Return HTTP Request 200*/
+	http_response_code(200);
+
+	function getFormatTextMessage($text)
+	{
+		$datas = [];
+		$datas['type'] = 'text';
+		$datas['text'] = $text;
+
+		return $datas;
+	}
+
+	function sentMessage($encodeJson,$datas)
+	{
+		$datasReturn = [];
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => $datas['url'],
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => "",
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 30,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => "POST",
+		  CURLOPT_POSTFIELDS => $encodeJson,
+		  CURLOPT_HTTPHEADER => array(
+		    "authorization: Bearer ".$datas['token'],
+		    "cache-control: no-cache",
+		    "content-type: application/json; charset=UTF-8",
+		  ),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		    $datasReturn['result'] = 'E';
+		    $datasReturn['message'] = $err;
+		} else {
+		    if($response == "{}"){
+			$datasReturn['result'] = 'S';
+			$datasReturn['message'] = 'Success';
+		    }else{
+			$datasReturn['result'] = 'E';
+			$datasReturn['message'] = $response;
+		    }
+		}
+
+		return $datasReturn;
+	}
+?>
