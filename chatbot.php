841689fc48236c0fd4e531306bad26316c60bda6
Thanakrit Sahachatchai <thanakrit.10197@gmail.com>
2020-09-28 13:55:08 +0700

    now

--- chatbot.php
+++ chatbot.php
@@ -91,7 +91,7 @@ $messages['messages'][0] =
             "paddingTop"=> "10px"
           ]
         ],
-        "width"=> "100%"
+        "height"=> "100%"
       ],
       "footer"=> [
         "type"=> "box",
